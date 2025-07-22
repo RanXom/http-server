f2d23c8b96b40695e8a22c938504cf8b7acb1465
RanXom <saiyedshizain@gmail.com>
2025-07-22 16:41:36 +0530

    feat: return Result from ThreadPool::new and introduce PoolCreationError

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,22 @@
+#[derive(Debug)]
+pub enum PoolCreationError {
+    ZeroSize,
+}
+
 pub struct ThreadPool;
 
 impl ThreadPool {
-    pub fn new(size: usize) -> ThreadPool {
-        assert!(size > 0);
+    pub fn new(size: usize) -> Result<ThreadPool, PoolCreationError> {
+        if size <= 0 {
+            return Err(PoolCreationError::ZeroSize)
+        }
 
-        ThreadPool
+        Ok(ThreadPool)
     }
 
     pub fn execute<F>(&self, f: F)
         where
-            F: FnOnce() + Send + 'static
+            F: FnOnce() + Send + 'static,
     {
         
     }
