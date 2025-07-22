6eefc897ec3a5012ce703fb0e239fddcceb74d4e
RanXom <saiyedshizain@gmail.com>
2025-07-22 16:11:48 +0530

    feat: introduce custom ThreadPool in main.rs

--- src/main.rs
+++ src/main.rs
@@ -7,12 +7,12 @@ use std::net::TcpListener;
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+    let pool = ThreadPool::new(4);
     
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("");
-        thread::spawn(|| {
+        pool.execute(|| {
             handle_connection(stream);
         });
     }
